891e649746b49df37861b7df22e1ef6eed3334a6
JavierMarcosFer <javiermarcosfer@gmail.com>
2022-09-08 17:14:15 -0500

    Pushed .gitignore and started implementation for manual shuffle start

---
+++ commands/adminStartshuffle.js
@@ -0,0 +1,13 @@
+const { SlashCommandBuilder } = require('discord.js');
+const shuffle = require('../shuffle-functions.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('startshuffle')
+		.setDescription('Manually start the weekly shuffle.'),
+	async execute(interaction) {
+		await interaction.deferReply();
+		shuffle.addSubmission(interaction);
+		return;
+	},
+};
